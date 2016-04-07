7f7021a6f2620cb2261f5cb6ff148e45c4f50e56
Eric Krawczyk <eric@docnetwork.org>
2016-04-07 18:16:31 -0400

    init npm and add basic express server

---
+++ server.js
@@ -0,0 +1,11 @@
+'use strict';
+var express = require('express');
+var port    = 3000;
+
+var app = express();
+
+// Implement your route here!
+
+app.listen(port);
+
+console.log("Listening on port: " + port.toString());
