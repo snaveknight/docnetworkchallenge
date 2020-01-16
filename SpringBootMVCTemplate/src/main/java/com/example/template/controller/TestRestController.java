d9cec79112f8f6d49709de944677f638e8a831ce
Max Evans <maxevans523@gmail.com>
2020-01-16 11:53:32 -0500

    got it to show a list of authors

--- SpringBootMVCTemplate/src/main/java/com/example/template/controller/TestRestController.java
+++ SpringBootMVCTemplate/src/main/java/com/example/template/controller/TestRestController.java
@@ -2,10 +2,18 @@ package com.example.template.controller;
 
 import com.example.template.data.MyClassJDBCDAO;
 import com.example.template.model.MyClass;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.List;
 
 @RestController
@@ -18,6 +26,24 @@ public class TestRestController {
     public List<MyClass> controllerMethod(Model model) {
 
         List<MyClass> dbData = jdbcDAO.getMyClassData();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            HttpEntity<String> httpEntity = new HttpEntity<>(new HttpHeaders());
+            RestTemplate restTemplate = new RestTemplate();
+            ResponseEntity<String> googleApi = restTemplate.exchange("https://www.googleapis.com/books/v1/volumes?q=it", HttpMethod.GET, httpEntity, String.class);
+            System.out.println(googleApi);
+            JsonNode projectNode = objectMapper.readTree(googleApi.getBody());
+            String nodePath = projectNode.path("items").path(0).path("volumeInfo").path("authors").path(0).asText();
+            System.out.println(nodePath);
+            int upperBound = projectNode.path("items").size();
+            for (int i = 0; i <= upperBound; i ++) {
+                System.out.println(projectNode.path("items").path(i).path("volumeInfo").path("authors").path(0).asText());
+            }
+        }
+        catch (Exception e) {
+
+        }
         return dbData;
     }
 
